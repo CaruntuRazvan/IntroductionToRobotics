d459ecc74d38642edb7e9d400753c056e4af34b3
Caruntu Razvan <115624498+CaruntuRazvan@users.noreply.github.com>
2023-10-23 18:09:39 +0300

    Add files via upload

---
+++ homework_1.ino
@@ -0,0 +1,48 @@
+const int blueLedPin = 11;
+const int greenLedPin = 10;
+const int redLedPin = 9;
+
+const int bluePotPin = A0;
+const int greenPotPin = A1;
+const int redPotPin = A2;
+
+unsigned int greenPotValue = 0;
+unsigned int bluePotValue = 0;
+unsigned int redPotValue = 0;
+
+unsigned int greenPinValue = 0;
+unsigned int bluePinValue = 0;
+unsigned int redPinValue = 0;
+
+const int minPot = 0;
+const int maxPot = 1023;
+const int minLed = 0;
+const int minMax = 255;
+
+void setup() {
+  pinMode(redPotPin, INPUT);
+  pinMode(greenPotPin, INPUT);
+  pinMode(bluePotPin, INPUT);
+
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //citirea valorilor potentiometrelor
+  greenPotValue = analogRead(greenPotPin);
+  bluePotValue = analogRead(bluePotPin);
+  redPotValue = analogRead(redPotPin); 
+
+  //calculare valoare analog pentru led
+  greenPinValue = map(greenPotValue, minPot, maxPot, minLed, minMax);
+  bluePinValue = map(bluePotValue, minPot, maxPot, minLed, minMax);
+  redPinValue = map(redPotValue, minPot, maxPot, minLed, minMax);
+
+  analogWrite(redLedPin, redPinValue); 
+  analogWrite(greenLedPin, greenPinValue);
+  analogWrite(blueLedPin, bluePinValue);
+  
+}
